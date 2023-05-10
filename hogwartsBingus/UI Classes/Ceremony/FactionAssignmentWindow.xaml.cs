4d80c153a2f7ebd179d1e0d58b5066684ccfea8b
PixelHole <pixelholeofficial@gmail.com>
2023-05-10 16:00:24 +0330

    added subject picking window and functionality

---
+++ hogwartsBingus/UI Classes/Ceremony/FactionAssignmentWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hogwartsBingus.UI_Classes.Ceremony
+{
+    /// <summary>
+    /// Interaction logic for FactionAssignmentWindow.xaml
+    /// </summary>
+    public partial class FactionAssignmentWindow : Window
+    {
+        public FactionAssignmentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AssignBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
